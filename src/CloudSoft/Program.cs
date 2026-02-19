98088a31eb83fdf6fcc7348690b52aeb0aac069b
Claes Fransson <81clafra@gafe.molndal.se>
2026-02-19 15:47:37 +0100

    Paste implement service layer code from assignment.

--- src/CloudSoft/Program.cs
+++ src/CloudSoft/Program.cs
@@ -1,7 +1,11 @@
+using CloudSoft.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddScoped<INewsletterService, NewsletterService>();
 
 var app = builder.Build();
 
