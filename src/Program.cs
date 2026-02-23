ac29f9a6c71d0fbad25aa7f87d498e0d93a72dd6
Claes Fransson <81clafra@gafe.molndal.se>
2026-02-23 12:30:09 +0100

    Paste implement the repository pattern code from assignment.

--- src/Program.cs
+++ src/Program.cs
@@ -1,3 +1,4 @@
+using CloudSoft.Repositories;
 using CloudSoft.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,6 +6,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// Register repository
+builder.Services.AddSingleton<ISubscriberRepository, InMemorySubscriberRepository>();
+
+// Register service (depends on repository)
 builder.Services.AddScoped<INewsletterService, NewsletterService>();
 
 var app = builder.Build();
